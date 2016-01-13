d5b5fa2ab29663df6fdc1c8a284dbbe6f75d074c
chefhoobajoob <chefhoobajoob@gmail.com>
2016-01-13 10:02:03 -0700

    Updating comment

--- verticle.js
+++ verticle.js
@@ -1,1 +1,4 @@
+// If NODE_PATH is not set correctly, vertx will be unable to find js-array, and launch will fail with
+// 'Cannot find module js-array'. If NODE_PATH is set, vertx will find js-array, but js-array's require
+// statement for './parser' will cause verticle launch to fail with 'Cannot find module ./parser'
 require('rql/js-array')
